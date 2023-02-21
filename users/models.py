81f21f42434e74f32876ea8c52612cb94a3d373f
Geferson <gefersonjefreey@gmail.com>
2023-02-21 09:46:47 -0300

    feat: add views of users and poetry in project

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    email = models.EmailField()
+    tel = models.CharField(max_length=20)
+    cpf = models.CharField(max_length=11, unique=True)
+    password = models.CharField(max_length=255)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+from django.contrib.auth.hashers import make_password
+from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Cria e salva um usuário com o email e a senha fornecidos
+        """
+        if not email:
+            raise ValueError('O Email é obrigatório')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.password = make_password(password) # Criptografa a senha
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Cria e salva um superusuário com o email e a senha fornecidos
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
